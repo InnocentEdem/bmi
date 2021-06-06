58021f38196aefb07e9316fc5e77619afb07c2fa
InnocentEdem <82113948+InnocentEdem@users.noreply.github.com>
2021-06-06 11:21:49 +0000

    made changes to display function

--- bmiMessage.js
+++ bmiMessage.js
@@ -3,6 +3,7 @@ const result = document.getElementsByClassName('result');
 const resultImage = document.getElementById('imgr');
 const resultText = document.getElementById('text');
 const callToAction = document.getElementById('btn');
+const imageList = document.getElementsByClassName('imgr');
 
 let bmi=()=>{
     let height = document.getElementById('height').value;
@@ -17,6 +18,9 @@ let bmi=()=>{
      if(isNaN(yourBmi)||!isFinite(yourBmi)){
         document.getElementById('height').style.border = 'solid 1px red';   
         document.getElementById('weight').style.border = 'solid 1px red';
+        Array.from(imageList).forEach(image => {
+            image.style.display='none';
+        });
         resultText.style.display = 'block';
         resultText.innerHTML = "Invalid Weight / Height !  Try again...";
         return ;
@@ -24,32 +28,33 @@ let bmi=()=>{
     if(yourBmi < 18.5){
         message="so you are Underweight";
         color = "rgb(255 208 22)";
-        imgSrc = 'images/underweight.png';
+        imgSrc = 1;
     }
     else if(yourBmi >= 18.5 && yourBmi <= 24.9){
         message = "so you have Normal Weight";
         color='rgb(83 131 19)'
-        imgSrc = 'images/normal-weight.png';
+        imgSrc = 2;
     }
-    else if(yourBmi >= 25 && yourBmi <= 29.9){
+    else if(yourBmi > 24.9 && yourBmi <= 29.9){
         message = "so you are Overweight";
         color="#fd802e";
-        imgSrc = 'images/overweight.png';
+        imgSrc = 3;
     }
-    else if(yourBmi >= 30 && yourBmi <= 34.9){
+    else if(yourBmi > 29.9 && yourBmi <= 34.9){
         message = "so you are Obese";
         color = "rgb(242 100 80)";
-        imgSrc = 'images/obese.png';
+        imgSrc = 4;
     }
      else {message = "so you are Extremely Obese";
-    color = "rgb(203 32 40)";
-    imgSrc = 'images/extreme.png';
-    } ``
-    
+        color = "rgb(203 32 40)";
+        imgSrc = 5;
+    } 
+    Array.from(imageList).forEach(image => {
+        image.style.display='none';
+    });
+    Array.from(imageList)[imgSrc-1].style.display='block';
     let finalMessage = `Your BMI is ${Math.round(yourBmi)}, ${message}.`;
-    resultImage.style.display = 'block';
     resultText.style.display = 'block';
-    resultImage.src = imgSrc;
     resultText.innerHTML = finalMessage;
     resultText.style.backgroundColor=color;
 }
